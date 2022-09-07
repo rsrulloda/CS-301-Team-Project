1c3a6b7fc34c21d61016d8bffef23dfb4d6e8d0e
rsrulloda <ronell.rulloda@gmail.com>
2022-09-06 22:37:53 -0700

    Added constructors, getters & setters

--- Transaction.cpp
+++ Transaction.cpp
@@ -2,36 +2,50 @@
 
 #include "Transaction.h"
 
-Transaction::Transaction() {
+#include <utility>
 
+Transaction::Transaction() {
+    setTransactionID(0);
+    setAssociateID(0);
+    setCashier("");
 }
 
 Transaction::Transaction(int transactionID, int associateID, string cashier) {
-
+    setTransactionID(transactionID);
+    setAssociateID(associateID);
+    setCashier(std::move(cashier));
 }
 
-int Transaction::getTransactionID() {
-    return 0;
+int Transaction::getTransactionID() const {
+    return transactionID;
 }
 
 void Transaction::setTransactionID(int transactionID) {
+    if(transactionID<0) {
+        throw "Invalid Transaction ID";
+    }
 
+    this->transactionID = transactionID;
 }
 
-int Transaction::getAssociateID() {
-    return 0;
+int Transaction::getAssociateID() const {
+    return associateID;
 }
 
 void Transaction::setAssociateID(int associateID) {
+    if(associateID<0) {
+        throw "Invalid Associate ID";
+    }
 
+    this->associateID = associateID;
 }
 
 string Transaction::getCashier() {
-    return std::string();
+    return cashier;
 }
 
 void Transaction::setCashier(string cashier) {
-
+    this->cashier = cashier;
 }
 
 void Transaction::Print() {
