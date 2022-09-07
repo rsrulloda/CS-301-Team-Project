2a7e20b6fd056f5deacb099423368dfdfc490ee5
rsrulloda <ronell.rulloda@gmail.com>
2022-09-06 23:58:49 -0700

    Added basic test

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,15 @@
 // Study Group 7 (Alex Milanez, John Edwin Karel Delgado, Ronell Sean Rulloda)
 
 #include <iostream>
+#include <Item.h>
+#include <Transaction.h>
+
 using namespace std;
 
 int main() {
+    Transaction transaction(001, 123, "name", CASH);
+
+    transaction.print();
+
     return 0;
 }
