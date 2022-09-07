b9364cc23d1e82014581ffc6c90610373203bedd
rsrulloda <ronell.rulloda@gmail.com>
2022-09-06 23:58:30 -0700

    Added transactionType

--- Transaction.cpp
+++ Transaction.cpp
@@ -3,6 +3,7 @@
 #include "Transaction.h"
 
 #include <utility>
+#include <iostream>
 
 Transaction::Transaction() {
     setTransactionID(0);
@@ -10,10 +11,11 @@ Transaction::Transaction() {
     setCashier("");
 }
 
-Transaction::Transaction(int transactionID, int associateID, string cashier) {
+Transaction::Transaction(int transactionID, int associateID, string cashier, TRANSACTION_TYPE transactionType) {
     setTransactionID(transactionID);
     setAssociateID(associateID);
     setCashier(std::move(cashier));
+    setTransactionType(transactionType);
 }
 
 int Transaction::getTransactionID() const {
@@ -48,6 +50,49 @@ void Transaction::setCashier(string cashier) {
     this->cashier = cashier;
 }
 
-void Transaction::Print() {
+TRANSACTION_TYPE Transaction::getTransactionType() {
+    return transactionType;
+}
 
+void Transaction::setTransactionType(TRANSACTION_TYPE transactionType) {
+    this->transactionType = transactionType;
+}
+
+void Transaction::print() {
+    cout << STORE_NAME << endl << endl;
+    cout << "Visit Us At " << WEBSITE << endl << endl;
+
+    cout << "STORE: " << STORE_ID << "\t";
+    cout << "REGISTER: " << REGISTER_ID << endl;
+    cout << "CASHIER: " << getCashier() << endl;
+    cout << "ASSOCIATE: " << getAssociateID() << endl;
+
+    cout << "-----------------------------" << endl;
+    cout << "CUSTOMER RECEIPT COPY" << endl;
+
+    // Insert Items Here
+    cout << "AD BOOST UNCAGED WHT/SIL 0\t139.99" << endl; // EXAMPLE
+
+    cout << "-----------------------------" << endl;
+    cout << "SUBTOTAL: " << endl;
+    cout << "SALES TAX: " << endl;
+    cout << "TOTAL: " << endl;
+    cout << "-----------------------------" << endl;
+    cout << "AMOUNT TENDERED" << endl;
+
+    if(getTransactionType()==CASH) {
+        cout << "cash" << endl; // Implement Later
+    } else {
+        cout << "credit/debit" << endl; // Implement Later
+    }
+
+    cout << "-----------------------------" << endl;
+    cout << "SALES TAX ANALYSIS" << endl;
+    cout << "CODE\tRATE%\tTAXABLE\tTAX" << endl;
+    cout << "LOCAL\t10.000\t" << "price" << "tax" << endl; // Implement Price & Tax later
+    cout << "-----------------------------" << endl;
+    cout << "tran: " << getTransactionID() << "\tDate & Time" << endl; // Implement Date & Time Later
+    cout << "-----------------------------" << endl;
+    cout << ADDRESS << endl;
+    cout << PHONE << endl;
 }
