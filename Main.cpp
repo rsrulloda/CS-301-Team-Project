66acc3619cf77f54a0c25287ab93661238e559e5
rsrulloda <ronell.rulloda@gmail.com>
2022-11-27 14:24:11 -0800

    Added Payment Type

--- Main.cpp
+++ Main.cpp
@@ -5,6 +5,8 @@
 #include <Item.h>
 #include <Transaction.h>
 
+#include "Libraries/rapidxml-1.13/rapidxml.hpp"
+
 using namespace std;
 
 string login(const string& filename, const string& employeeID) {
@@ -12,7 +14,7 @@ string login(const string& filename, const string& employeeID) {
     string tempEmployeeID;
     string tempCashier;
 
-    std::ifstream file(filename);
+    std::ifstream file(filename); // Opens file
     if(!file.is_open()) throw std::invalid_argument("File Failed to Open");
     file >> employeeCount;
 
@@ -32,11 +34,110 @@ string login(const string& filename, const string& employeeID) {
 }
 
 void newTransaction(const string& cashier, const string& employeeID) {
-    Transaction transaction(001, employeeID, cashier, CASH); // test
-    transaction.print();
+    int transactionCount = 0;
+    int UPC;
+    char option;
+    bool loop = true;
+
+    while (loop) {
+        std::fstream file("../Database/history.csv");
+        if(!file.is_open()) throw std::invalid_argument("File Failed to Open");
+        file >> transactionCount;
+        transactionCount = 1;
+        Transaction transaction(transactionCount, employeeID, cashier);
+        file.close();
+
+        cout << "\nPlease Select an Option:\n1. Scan\n2. Print\n3. Exit" << endl;
+        cin >> option;
+
+        switch (option) {
+            case '1': { // Scan
+                cout << "\nPlease enter UPC:";
+                cin >> UPC;
+                transaction.scan(UPC);
+                break;
+            }
+
+            case '2': { // Print Receipt
+                string paymentType;
+                char option2;
+
+                cout << "\nTotal: " << transaction.getTotal() << endl;
+                cout << "\nPlease Choose Payment Method: \n1. CREDIT\n2. DEBIT\n3. CASH\n4. CANCEL" << endl;
+                cin >> option2;
+
+                switch(option2) {
+                    case '1': { // CREDIT
+                        paymentType = "CREDIT";
+                        int card;
+
+                        cout << "\nPAYMENT METHOD: CREDIT";
+                        cout << "\nPlease Insert Card:" << endl;
+                        cin >> card;
+                        break;
+                    }
+
+                    case '2': { // DEBIT
+                        paymentType = "DEBIT";
+                        int card;
+
+                        cout << "\nPAYMENT METHOD: DEBIT";
+                        cout << "\nPlease Insert Card:" << endl;
+                        cin >> card;
+                        break;
+                    }
+
+                    case '3': { // CASH
+                        paymentType = "CASH";
+                        double cash;
+
+                        cout << "\nPAYMENT METHOD: CASH" << endl;
+                        cout << "\nPlease Insert Cash:" << endl;
+                        cin >> cash;
+
+                        double change = cash-transaction.getTotal();
+                        cout << "CHANGE: $" << change << endl;
+
+                        break;
+                    }
+
+                    case '4': { // CANCEL
+                        cout << "\nCANCEL" << endl;
+                        break;
+                    }
+
+                    default: {
+                        cout << "Please enter valid option." << endl;
+                        break;
+                    }
+                }
+
+                transaction.print(paymentType);
+
+                std::ofstream file2("../Database/history.csv");
+                if(!file2.is_open()) throw std::invalid_argument("File Failed to Open");
+                file2 << transactionCount;
+                file2.close();
+                loop = false;
+                break;
+            }
+
+            case '3': { // Exit
+                cout << "\nExited" << endl;
+                loop = false;
+                break;
+            }
+
+            default: {
+                cout << "Please enter valid option." << endl;
+                break;
+            }
+        }
+    }
 }
 
 int main() {
+
     int option;
     bool appOn = true;
 
@@ -49,7 +150,7 @@ int main() {
                 string employeeID;
                 cout << "\nPlease enter employee ID:" << endl;
                 cin >> employeeID;
-                string cashier = login("../Database/logins.txt", employeeID);
+                string cashier = login("../Database/logins.csv", employeeID);
 
                 char option2;
                 bool loggedIn = true;
@@ -64,8 +165,18 @@ int main() {
                             break;
                         }
 
-                        case '2': { // Implement Later
-                            cout << "Transaction History" << endl;
+                        case '2': { // Transaction History
+                            int historyCount;
+
+                            std::fstream file2("../Database/history.csv",ios::out | ios::app);
+                            if(!file2.is_open()) throw std::invalid_argument("File Failed to Open");
+                            cin >> historyCount;
+
+                            for(int i=0;i<historyCount;i++) {
+                                // cout <<                          IMPLEMENT LATER
+                            }
+
+                            file2.close();
                             break;
                         }
 
